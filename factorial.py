2fc36517c0e81be91978ad353b7161756ca2b799
srivanisrii <144410393+srivanisrii@users.noreply.github.com>
2023-09-08 09:46:01 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,7 @@
+def recur_factorial(n):
+  if n==1:
+    return n
+  else:
+    return n*recur_factorial(n-1)
+num = int(input("Enter a number:"))
+print ("The factorial of ",num,"is",recur_factorial(num))
